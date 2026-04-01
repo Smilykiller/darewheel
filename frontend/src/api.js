9a656dbf80ecd692dc3352ffb3440322f484ac8d
Smilykiller <limalkavinesh18@gmail.com>
2026-04-01 23:07:20 +0530

    currently working

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://darewheel-backend-xyz.onrender.com/api',
 });
 
 // Automatically attach the user's token to every request
